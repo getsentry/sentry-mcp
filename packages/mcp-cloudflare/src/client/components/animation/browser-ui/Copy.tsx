78cb0d73d11caf6794795f054d2c55c269ea1827
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-10-06 13:32:43 -0400

    branded meta key asset, adjust keycap animation timing

--- packages/mcp-cloudflare/src/client/components/animation/browser-ui/Copy.tsx
+++ packages/mcp-cloudflare/src/client/components/animation/browser-ui/Copy.tsx
@@ -9,16 +9,16 @@ export default function Copy({ step }: { step?: number }) {
       >
         {/* <div className="absolute bottom-0 left-1/2 h-[69%] w-[72.5%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl bg-[#160f24] perspective-distant" /> */}
         <div className="[mask-image:url('/keycap-⌘.png')] [mask-size:100%] bg-clip-content size-fit relative [filter:drop-shadow(inset_0_-1rem_1rem_rgba(0,0,0,1))] translate-y-2">
-          <div className="absolute bottom-0 left-1/2 h-[69%] w-[72.5%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl bg-[#160f24] perspective-distant" />
+          <div className="absolute bottom-0 left-1/2 h-[69%] w-[75%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl rounded-tr-2xl bg-[#160f2488] perspective-distant -z-10" />
           <img
             className={`${
               step === 0 && "animate-keycap-inner-meta"
             } bg-clip-content`}
             src="/keycap-⌘.png"
             alt="keycap-⌘"
-            style={{ ["--delay" as any]: "1.05s" }}
+            style={{ ["--delay" as any]: "1.20s" }}
           />
-          <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-transparent to-transparent" />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-30% via-transparent to-transparent z-10" />
         </div>
       </div>
       <div
@@ -29,16 +29,16 @@ export default function Copy({ step }: { step?: number }) {
       >
         {/* <div className="absolute bottom-0 left-1/2 h-[69%] w-[72.5%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl bg-[#160f24] perspective-distant" /> */}
         <div className="[mask-image:url('/keycap-c.png')] [mask-size:100%] bg-clip-content size-fit relative [filter:drop-shadow(inset_0_-1rem_1rem_rgba(0,0,0,1))] translate-y-2">
-          <div className="absolute bottom-0 left-1/2 h-[69%] w-[72.5%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl bg-[#160f24] perspective-distant" />
+          <div className="absolute bottom-0 left-1/2 h-[69%] w-[72.5%] -translate-x-[51%] rotate-x-50 rotate-z-27 rounded-3xl rounded-br-2xl bg-[#160f2488] perspective-distant -z-10" />
           <img
             className={`${
               step === 0 && "animate-keycap-inner"
             } bg-clip-content`}
             src="/keycap-c.png"
             alt="keycap-c"
-            style={{ ["--delay" as any]: "1.30s" }}
+            style={{ ["--delay" as any]: "1.35s" }}
           />
-          <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-transparent to-transparent" />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-30% via-transparent to-transparent z-10" />
         </div>
       </div>
     </div>
