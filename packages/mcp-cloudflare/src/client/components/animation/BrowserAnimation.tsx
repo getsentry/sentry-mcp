fe9aa27afca1ee5034a8404db1216fdecba8cc3d
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-10-28 06:47:08 -0400

    better validations summary step visual icons, cleanup BrowserAnimation component

--- packages/mcp-cloudflare/src/client/components/animation/BrowserAnimation.tsx
+++ packages/mcp-cloudflare/src/client/components/animation/BrowserAnimation.tsx
@@ -9,17 +9,11 @@ export default function BrowserAnimation({
   globalIndex: number;
 }) {
   return (
-    <div
-      className={`relative h-full w-full ${
-        globalIndex >= 2 && "overflow-hidden"
-      } hidden md:block rounded-3xl bg-dots`}
-    >
+    <div className="relative h-full w-full hidden md:block bg-dots">
       <IDEWindow step={globalIndex} />
       <BrowserWindow step={globalIndex} />
       <LoadingSquares step={globalIndex} />
       <ValidationSummary step={globalIndex} />
-      {/*<div id="window3-browser" className="absolute flex flex-col pr-1 pb-1 w-full h-full bg-600 rounded-2xl border border-white/10 scale-90 bottom-0 origin-bottom">
-      </div>*/}
     </div>
   );
 }
--- packages/mcp-cloudflare/src/client/components/animation/tests.tsx
+++ packages/mcp-cloudflare/src/client/components/animation/tests.tsx
@@ -1,3 +1,4 @@
+import { Maximize } from "lucide-react";
 import { CheckCheck } from "lucide-react";
 
 export default function ValidationSummary({ step }: { step?: number }) {
@@ -9,14 +10,28 @@ export default function ValidationSummary({ step }: { step?: number }) {
           : "scale-0 opacity-0 blur-xl"
       } pointer-events-none absolute top-0 left-1/2 -translate-x-1/2 transition-all gap-8`}
     >
-      <div className="flex items-center gap-6 text-2xl font-bold font-mono">
+      <div className="flex items-center gap-6 text-2xl font-bold font-mono relative">
         <div
-          className={`rounded-full bg-gradient-to-br from-lime-400 to-lime-950 p-3 ${
+          className={`rounded-lg absolute left-0 inset-y-0 size-16 bg-lime-200 bg-[repeating-linear-gradient(45deg,#0008,#0008_4px,#0004_4px,#0004_10px)] scale-95 ${
+            step === 5
+              ? "opacity-100 delay-1000 duration-500 translate-2"
+              : "opacity-0"
+          }`}
+        />
+        <div
+          className={`rounded-lg relative bg-gradient-to-br from-lime-900 to-lime-950 p-3 ${
             step === 5 ? "scale-100 delay-1000 duration-500" : "scale-200"
           } origin-top-left`}
         >
+          <Maximize
+            className={`absolute -inset-2 size-[125%] stroke-lime-200/50 stroke-[0.5px] ${
+              step === 5
+                ? "scale-100 opacity-100 delay-700 duration-500"
+                : "scale-75 opacity-0"
+            }`}
+          />
           <CheckCheck
-            className={`size-10 text-white/50 ${
+            className={`size-10 text-lime-200 ${
               step === 5
                 ? "scale-100 opacity-100 blur-none delay-200 duration-500"
                 : "scale-0 opacity-0 blur-xl"
@@ -26,21 +41,35 @@ export default function ValidationSummary({ step }: { step?: number }) {
         <span
           className={`${
             step === 5
-              ? "opacity-100 translate-x-0 duration-300 delay-1500"
-              : "opacity-0 translate-x-4"
+              ? "opacity-100 translate-x-0 duration-300 delay-1250"
+              : "opacity-0 -translate-x-4"
           } transition-all`}
         >
-          pnpm run tsc
+          <span className="opacity-50">pnpm run</span> tsc
         </span>
       </div>
-      <div className="flex items-center gap-6 text-2xl font-bold font-mono">
+      <div className="flex items-center gap-6 text-2xl font-bold font-mono relative">
         <div
-          className={`rounded-full bg-gradient-to-br from-lime-400 to-lime-950 p-3 ${
+          className={`rounded-lg absolute left-0 inset-y-0 size-16 bg-lime-200 bg-[repeating-linear-gradient(45deg,#0008,#0008_4px,#0004_4px,#0004_10px)] scale-95 ${
+            step === 5
+              ? "opacity-100 delay-1200 duration-500 translate-2"
+              : "opacity-0"
+          }`}
+        />
+        <div
+          className={`rounded-lg relative bg-gradient-to-br from-lime-900 to-lime-950 p-3 ${
             step === 5 ? "opacity-100 delay-1200 duration-500" : "opacity-0"
           }`}
         >
+          <Maximize
+            className={`absolute -inset-2 size-[125%] stroke-lime-200/50 stroke-[0.5px] ${
+              step === 5
+                ? "scale-100 opacity-100 delay-1200 duration-500"
+                : "scale-75 opacity-0"
+            }`}
+          />
           <CheckCheck
-            className={`size-10 text-white/50 ${
+            className={`size-10 text-lime-200 ${
               step === 5
                 ? "scale-100 opacity-100 blur-none delay-200 duration-500"
                 : "scale-0 opacity-0 blur-xl"
@@ -50,21 +79,35 @@ export default function ValidationSummary({ step }: { step?: number }) {
         <span
           className={`${
             step === 5
-              ? "opacity-100 translate-x-0 duration-300 delay-1700"
-              : "opacity-0 translate-x-4"
+              ? "opacity-100 translate-x-0 duration-300 delay-1450"
+              : "opacity-0 -translate-x-4"
           } transition-all`}
         >
-          pnpm run lint
+          <span className="opacity-50">pnpm run</span> lint
         </span>
       </div>
-      <div className="flex items-center gap-6 text-2xl font-bold font-mono">
+      <div className="flex items-center gap-6 text-2xl font-bold font-mono relative">
         <div
-          className={`rounded-full bg-gradient-to-br from-lime-400 to-lime-950 p-3 ${
+          className={`rounded-lg absolute left-0 inset-y-0 size-16 bg-lime-200 bg-[repeating-linear-gradient(45deg,#0008,#0008_4px,#0004_4px,#0004_10px)] scale-95 ${
+            step === 5
+              ? "opacity-100 delay-1400 duration-500 translate-2"
+              : "opacity-0"
+          }`}
+        />
+        <div
+          className={`rounded-lg relative bg-gradient-to-br from-lime-900 to-lime-950 p-3 ${
             step === 5 ? "opacity-100 delay-1400 duration-500" : "opacity-0"
           }`}
         >
+          <Maximize
+            className={`absolute -inset-2 size-[125%] stroke-lime-200/50 stroke-[0.5px] ${
+              step === 5
+                ? "scale-100 opacity-100 delay-1400 duration-500"
+                : "scale-75 opacity-0"
+            }`}
+          />
           <CheckCheck
-            className={`size-10 text-white/50 ${
+            className={`size-10 text-lime-200 ${
               step === 5
                 ? "scale-100 opacity-100 blur-none delay-200 duration-500"
                 : "scale-0 opacity-0 blur-xl"
@@ -74,11 +117,11 @@ export default function ValidationSummary({ step }: { step?: number }) {
         <span
           className={`${
             step === 5
-              ? "opacity-100 translate-x-0 duration-300 delay-2000"
-              : "opacity-0 translate-x-4"
+              ? "opacity-100 translate-x-0 duration-300 delay-1750"
+              : "opacity-0 -translate-x-4"
           } transition-all`}
         >
-          pnpm run tests
+          <span className="opacity-50">pnpm run</span> tests
         </span>
       </div>
     </div>
