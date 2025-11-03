8df18a9b8ecba51b32322b34f60d89b6ad1cf4be
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-11-03 16:22:22 -0500

    fix missed step styles

--- packages/mcp-cloudflare/src/client/components/animation/TerminalAnimation.tsx
+++ packages/mcp-cloudflare/src/client/components/animation/TerminalAnimation.tsx
@@ -294,10 +294,10 @@ export default function TerminalAnimation() {
           <div className="relative">
             <button
               type="button"
-              className={`border group/replay border-white/20 bg-white/15 hover:bg-white/30 active:bg-white/50 active:duration-75 duration-300 absolute -top-14 rounded-full px-3 py-1 left-0 z-50 cursor-pointer hover:duration-300 hover:delay-0 text-nowrap ${
+              className={`border group/replay border-white/20 bg-white/15 hover:bg-white/30 active:bg-white/50 active:duration-75 duration-300 absolute -top-14 rounded-full px-3 py-1 left-0 z-20 cursor-pointer hover:duration-300 hover:delay-0 text-nowrap ${
                 currentIndex === 5
                   ? "opacity-100 translate-y-0 delay-3000 duration-500 pointer-events-auto"
-                  : "opacity-0 translate-y-1/2 pointer-events-"
+                  : "opacity-0 translate-y-1/2 pointer-events-none"
               }`}
               onClick={() => restart()}
             >
